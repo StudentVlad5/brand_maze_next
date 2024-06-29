8613e7c409c1195b391b2cb8a1b485e2c6d5e1f6
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2024-06-29 23:16:02 +0300

    start

---
+++ src/app/(admin)/contact/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h1 className="Contacts">Home page</h1>
+    </main>
+  );
+}
---
+++ src/app/(admin)/services/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Services</h1>
+    </main>
+  );
+}
